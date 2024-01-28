95a0cba42bdadfd0ff0b6dec78eb89090f22e838
d4niel <olayajuandaniel8@gmail.com>
2024-01-27 21:35:05 -0500

    text-gray

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,18 +3,19 @@
       <!-- svelte-ignore a11y-missing-attribute -->
       
 
-        <img class="w-60 h-2/4">
+        <x-application-logo class="w-20 h-20"></x-application-logo>
+        <span class="text-gray-200"> Scroud</span>
       
     </div>
     
     
-    <div class="flex-end">
-        
+    <div class="flex flex-between">
+         
         <div>
             @auth
-            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-200 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
             @else
-            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+            <a href="{{ route('login') }}" class="font-semibold text-gray-200 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
             
             @if (Route::has('register'))
             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
