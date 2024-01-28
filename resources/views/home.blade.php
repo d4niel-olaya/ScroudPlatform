76971881f450463b9f7bba6495fee2240eb51be5
d4niel <olayajuandaniel8@gmail.com>
2024-01-27 20:17:23 -0500

    navbar 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,9 +20,6 @@
     </head>
     <body>
         <div class="sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            <x-navbar>
-                
-            </x-navbar>
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
