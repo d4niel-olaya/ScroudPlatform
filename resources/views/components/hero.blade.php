9f8b75b03dd7f436611031cda6b31f2fb218ca49
d4niel <olayajuandaniel8@gmail.com>
2024-01-27 22:52:26 -0500

    hero fix

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,8 +1,8 @@
 <div class="hero min-h-screen">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold text-gray-200">Scroud</h1>
-        <p class="py-6 text-gray-200">The Cloud Storage for developers</p>
+        <h1 class="text-5xl font-bold text-gray-200">The Cloud Storage for developers</h1>
+        <p class="py-6 text-gray-200">Manage your buckets easily using an API key</p>
         <button class="btn btn-secondary">Get Started</button>
       </div>
     </div>
